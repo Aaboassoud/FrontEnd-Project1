139518ef1410306215190111b33aa551e70223fb
Aaboassoud <a.aboassoud@gmail.com>
2022-06-25 04:15:22 +0300

    4th commit

--- JavaScripts/main.js
+++ JavaScripts/main.js
@@ -1,7 +1,30 @@
+// ------------------ actions ------------------
+function login() {
+    let username = document.getElementById('username').value;
+    let password = document.getElementById('password').value;
+    localStorage.setItem('username', username);
+    localStorage.setItem('password', password);
+    showHome();
+}
+
+// ------------------ pages ------------------
 function showHome() {
     document.getElementById("Home").className = "d-block";
     document.getElementById("Login").className = "d-none";
     document.getElementById("Register").className = "d-none";
+    let username = localStorage.getItem('username');
+    if (username != null) {
+        let loginBtn    = document.getElementById("login-btn")
+        let registerBtn = document.getElementById("register-btn")
+        let welId       = document.getElementById("welcome")
+        loginBtn.className    = "d-none"
+        registerBtn.className = "d-none"
+        let welcome = document.createElement("h3")
+        welcome.className = "text-warning"
+        welcome.id = "welcome-id"
+        welcome.innerText = `Welcome, ${username}`
+        welId.insertBefore(welcome,loginBtn)
+    }
     return false;
 }
 
@@ -18,3 +41,4 @@ function showRegister() {
     document.getElementById("Home").className = "d-none";
     return false;
 }
+
